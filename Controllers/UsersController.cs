c50b0231f47859647b4655a30c11e1d18f8b8521
Ruel Boquiren <ruel.boquiren@gmail.com>
2017-08-15 17:57:58 +0800

    userdelete

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -101,6 +101,17 @@ namespace MyTemplate.Controllers
             
         }
 
+        [Authorize(ActiveAuthenticationSchemes="Bearer")]
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("/api/users/delete")]
+         public async Task<IEnumerable<UserResource>> DeleteUser(int id)
+        {
+            var user = await db.Users.Where(u => u.Id.Equals(id)).Include(u => u.Roles).SingleOrDefaultAsync();
+            
+            db.Users.Remove(user);
+            var users = await db.Users.Include(u => u.Roles).ToListAsync();
+            return mapper.Map<List<User>, List<UserResource>>(users);
+        }
         
 
     }
